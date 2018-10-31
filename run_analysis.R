2db7c339738e0d5ff0c68ade666f7a5d98f39a48
DaniloMetal <38691347+DaniloMetal@users.noreply.github.com>
2018-10-31 00:05:24 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,78 +1,57 @@
 ## Setting workspace
-
 setwd(file.path("C:","Users","danil","Desktop","Coursera","Data Science","Getting and Cleaning Data","Week4","Course Project","UCI HAR Dataset"));
 
-
 #### Calling libraries
-
-library(tidyr);
-library(dplyr);
 library(reshape2);
 library(data.table);
 
 #### Reading Data Sets
 
 ## 1 Merges the training and the test sets to create one data set.
-
 X_train <- read.table("train/X_train.txt", quote="\"", stringsAsFactors=FALSE);
-
 X_test <- read.table("test/X_test.txt", quote="\"", stringsAsFactors=FALSE);
+features <- read.table("features.txt", quote="\"", stringsAsFactors=FALSE);
+## 4. Appropriately labels the data set with descriptive variable names.
+names(X_train) <- features$V2;
+names(X_test) <- features$V2;
 
+##### Y activity related
 
-## 1.2
-features.duplicated <- read.table("features.txt", quote="\"",stringsAsFactors=FALSE);
-
-##features.duplicated[duplicated(features.duplicated$V2),]
-##     V1                               V2
-##317 317       fBodyAcc-bandsEnergy()-1,8 
-##Many records duplicated 
-
-features <- make.names(features.duplicated$V2, unique = TRUE)  ## Removing duplicate rows and not allowed characters
-
-
-subject_train <- read.table("train/subject_train.txt", quote="\"", stringsAsFactors=FALSE);
-
-subject_test <- read.table("test/subject_test.txt", quote="\"", stringsAsFactors=FALSE);
+y_train <- read.table("train/y_train.txt", quote="\"", stringsAsFactors=FALSE);
+y_test <- read.table("test/y_test.txt", quote="\"", stringsAsFactors=FALSE);
+## 4. Appropriately labels the data set with descriptive variable names.
+names(y_train) <- "activity.labels";
+names(y_test) <- "activity.labels";
 
 activity_labels <- read.delim("activity_labels.txt",sep = ' ',header = F); ## 'activity_labels.txt': Links the class labels with their activity.
 
 
+### ID of the volunteer
 
-X_bind_rows <- bind_rows(X_train, X_test); ### Merging two datasets to crete one data set "point 1"   
-
+subject_train <- read.table("train/subject_train.txt", quote="\"", stringsAsFactors=FALSE);
+subject_test <- read.table("test/subject_test.txt", quote="\"", stringsAsFactors=FALSE);
 ## 4. Appropriately labels the data set with descriptive variable names.
-
-names(X_bind_rows) <- features;            
-
-subject_bind_rows <- bind_rows(subject_train, subject_test);
-
-subject_activity <- merge(activity_labels, subject_bind_rows, by.x = "V1", by.y = "V1", all=TRUE); ## Joint description with activities
-names(subject_activity) <- c("subject.id","activity.desc");
-
-## 3. Uses descriptive activity names to name the activities in the data set
-
-data_bind <- cbind(as.data.table(X_bind_rows),(subject_activity));
-
-##### Y
-
-y_train <- read.table("train/y_train.txt", quote="\"", stringsAsFactors=FALSE);
-y_test <- read.table("train/y_train.txt", quote="\"", stringsAsFactors=FALSE);
-
-y_bind <- bind_rows(y_train, y_test);  ### Merging two datasets to crete one data set "point 1"
-
-## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-
-mean <- select(data_bind, contains("mean")); ## Select mean values
-std <- select(data_bind, contains("std"));   ## Select standard deviation values 
+names(subject_train) <- "volunteerID";
+names(subject_test) <- "volunteerID";
 
 
-mean.std <- cbind(as.data.table(mean),(std));
-DataSet.1 <- cbind(as.data.table(mean.std), (subject_activity));
+train <- cbind(subject_train, y_train, X_train);
+test <- cbind(subject_test, y_test, X_test);
+data_bind <- rbind(train, test);               ### Merging two datasets to crete one data set "point 1"
 
-write.csv(DataSet.1, "DataSet.1.csv");
 
-##5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+mean.std.regexp <- grepl("mean\\(\\)", names(data_bind)) | grepl("std\\(\\)", names(data_bind))
+mean.std.regexp [c(1,2)] <- TRUE ## Force true to keep the primary and second colunm to not be removed 
+data_bind <- data_bind[, mean.std.regexp] # Filter to keep just mean and std colunms
 
-DataSet.2 <- na.omit(DataSet.1) ## Function for remove rows with missing values on columns in order to tyde the dataset 
 
-write.csv(DataSet.2,"DataSet.2.csv"); ## ¡Final Tidy Data!
+## 4.	Appropriately labels the data set with descriptive variable names.
+data_bind$activity.labels <- factor(data_bind$activity.labels, labels=c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"));
+
+### Normalizing and aggregator to tidy the data
+nrmz <- melt(data_bind, id=c("volunteerID","activity.labels"));  ##  normalizing the data.. Similar to gather()
+Datastet.Tidy <- dcast(nrmz, volunteerID+activity.labels ~ variable, mean); ## aggregating and getting mean
+
+## Finally!! Write final Dataset :)
+
+write.table(Datastet.Tidy, file = "Datastet.Tidy.txt", sep = "\t", row.names = FALSE)
